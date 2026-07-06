1a4a39944c83379e5423b25d158e3efefb56f086
crew19 <nstdev22@gmail.com>
2026-07-07 01:42:07 +0530

    change default port

--- src/config/env.js
+++ src/config/env.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   /** Please add the values here in the production environment */
-  port: process.env.PORT,
+  port: process.env.PORT || 5000,
   mongoUri: process.env.MONGODB_URI,
   jwtSecret: process.env.JWT_SECRET,
   jwtAccessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
